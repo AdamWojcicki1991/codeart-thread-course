608ad2929bb829154116a70f8d2924df132e313e
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-06-25 23:29:51 +0200

    Use Join method - CodeArt module 2.7

---
+++ codeart-thread/src/main/java/com/codeart/thread/JoinLesson.java
@@ -0,0 +1,27 @@
+package com.codeart.thread;
+
+public class JoinLesson {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " = Starting...");
+
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    for (int i = 0; i < 10; i++) {
+                        System.out.println(Thread.currentThread().getName() + " = I am running in a thread...");
+                        Thread.sleep(1_000);
+                    }
+                } catch (InterruptedException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+
+        Thread t0 = new Thread(task);
+        t0.start();
+        t0.join(2_000);
+
+        System.out.println(Thread.currentThread().getName() + " = Done");
+    }
+}
