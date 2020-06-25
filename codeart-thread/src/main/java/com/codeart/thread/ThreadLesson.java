f179070019af294a4afd17e621dc609a981c2802
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-06-25 14:31:43 +0200

    Create new Thread - CodeArt module 2.4

--- codeart-thread/src/main/java/com/codeart/thread/ThreadLesson.java
+++ codeart-thread/src/main/java/com/codeart/thread/ThreadLesson.java
@@ -2,6 +2,16 @@ package com.codeart.thread;
 
 public class ThreadLesson {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println(Thread.currentThread().getName() + " - Hello, World!");
+
+        Thread thread = new Thread("WorkingThread") {
+            @Override
+            public void run() {
+                System.out.println(Thread.currentThread().getName() + " - I am running in a thread");
+            }
+        };
+        thread.start();
+
+        System.out.println(Thread.currentThread().getName() + " - Done");
     }
 }
