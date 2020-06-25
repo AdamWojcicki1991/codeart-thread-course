94820faeebff423687e90c2a1f65058c3fcae803
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-06-25 16:22:39 +0200

    Create new Daemon - CodeArt module 2.6

---
+++ codeart-thread/src/main/java/com/codeart/thread/DaemonLesson.java
@@ -0,0 +1,45 @@
+package com.codeart.thread;
+
+import java.text.NumberFormat;
+
+public class DaemonLesson {
+
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Starting...");
+
+        Runnable diagnosticTask = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    while (true) {
+                        Runtime runtime = Runtime.getRuntime();
+                        NumberFormat format = NumberFormat.getInstance();
+
+                        StringBuilder stringBuilder = new StringBuilder();
+                        long maxMemory = runtime.maxMemory();
+                        long allocatedMemory = runtime.totalMemory();
+                        long freeMemory = runtime.freeMemory();
+
+                        stringBuilder.append("free memory: " + format.format(freeMemory / 1024) + "\n");
+                        stringBuilder.append("allocated memory: " + format.format(allocatedMemory / 1024) + "\n");
+                        stringBuilder.append("max memory: " + format.format(maxMemory / 1024) + "\n");
+                        stringBuilder.append("total free memory: " + format.format((freeMemory + maxMemory - allocatedMemory) / 1024) + "\n");
+                        System.out.println(stringBuilder.toString());
+                        Thread.sleep(1_000);
+                    }
+                } catch (InterruptedException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+        Thread diagnosticThread = new Thread(diagnosticTask);
+        diagnosticThread.setDaemon(true);
+        diagnosticThread.start();
+
+        System.out.println("Is Daemon: " + diagnosticThread.isDaemon());
+
+        Thread.sleep(5_000);
+
+        System.out.println("Done...");
+    }
+}
