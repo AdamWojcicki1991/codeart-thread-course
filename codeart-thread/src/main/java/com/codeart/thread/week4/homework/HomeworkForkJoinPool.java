22c6c27a42917e8a4fee95422db47f5e49a7af77
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-08-13 01:45:00 +0200

    Homework Thread Pools - CodeArt module 4.11

---
+++ codeart-thread/src/main/java/com/codeart/thread/week4/homework/HomeworkForkJoinPool.java
@@ -0,0 +1,61 @@
+package com.codeart.thread.week4.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.ForkJoinTask;
+import java.util.concurrent.RecursiveTask;
+
+public class HomeworkForkJoinPool {
+    private static String extractNumber(final String str) {
+        if (str == null || str.isEmpty()) return "0";
+        StringBuilder stringBuilder = new StringBuilder();
+        for (char character : str.toCharArray()) {
+            if (Character.isDigit(character)) {
+                stringBuilder.append(character);
+            }
+        }
+        return stringBuilder.toString();
+    }
+
+    private static List<CountingTask> getCountingTasks(final String initialString) {
+        List<CountingTask> tasks = new ArrayList<>();
+        tasks.add(new CountingTask((initialString.substring(0, initialString.length() / 2))));
+        tasks.add(new CountingTask((initialString.substring(initialString.length() / 2))));
+        return tasks;
+    }
+
+    private final static class CountingTask extends RecursiveTask<Integer> {
+        private final String initialString;
+
+        private CountingTask(final String initialString) {
+            this.initialString = extractNumber(initialString);
+        }
+
+        @Override
+        protected Integer compute() {
+            System.out.println("[" + Thread.currentThread().getName() + "] Computing value: " + initialString);
+            if (initialString.isEmpty() || initialString.length() == 1) {
+                return Integer.parseInt(initialString);
+            } else {
+                List<CountingTask> tasks = getCountingTasks(initialString);
+                tasks.forEach(ForkJoinTask::fork);
+                return tasks.stream().mapToInt(ForkJoinTask::join).sum();
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("[" + Thread.currentThread().getName() + "] START");
+
+        ForkJoinPool joinPool = ForkJoinPool.commonPool();
+
+        String stringInputValue = "123456";
+
+        Integer sum = joinPool.invoke(new CountingTask(stringInputValue));
+
+        System.out.println("Result is: " + sum);
+
+        System.out.println("[" + Thread.currentThread().getName() + "] DONE");
+    }
+}
---
+++ codeart-thread/src/main/java/com/codeart/thread/week4/homework/HomeworkThreadPool.java
@@ -0,0 +1,67 @@
+package com.codeart.thread.week4.homework;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.IntStream;
+
+public class HomeworkThreadPool {
+    private static String diagnosticMonitor(final ThreadPoolExecutor executor, final AtomicLong lostTaskCount) {
+        return "[" + Thread.currentThread().getName() +
+                "] Queue capacity is full - [" +
+                "Task count: " + executor.getTaskCount() +
+                " | Pool size: " + executor.getPoolSize() +
+                " | Keep Alive Time [sec]: " + executor.getKeepAliveTime(TimeUnit.SECONDS) +
+                " | Queue size: " + executor.getQueue().size() +
+                " | Task lost count: " + lostTaskCount + "]";
+    }
+
+    private static void shutdown(Counter counter, ThreadPoolExecutor threadPoolExecutor) {
+        if (threadPoolExecutor.isShutdown()) {
+            System.out.println("RejectedExecution shutdown - Task lost count was to big: " + counter.value);
+        } else {
+            System.out.println("Application shutdown - Task lost count was under 10: " + counter.value);
+            threadPoolExecutor.shutdown();
+        }
+    }
+
+    private final static class Counter {
+        private final AtomicLong value = new AtomicLong(0);
+
+        private long increment() {
+            return value.incrementAndGet();
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println("[" + Thread.currentThread().getName() + "] START");
+
+        final Counter counter = new Counter();
+
+        ThreadPoolExecutor threadPoolExecutor = new ThreadPoolExecutor(
+                1, 4,
+                30L, TimeUnit.SECONDS,
+                new LinkedBlockingQueue<>(10),
+                (task, executor) -> {
+                    if (counter.increment() > 9) executor.shutdown();
+                    System.out.println(diagnosticMonitor(executor, counter.value));
+                });
+
+        Runnable task = () -> {
+            try {
+                System.out.println("[" + Thread.currentThread().getName() + "] Task in progress...");
+                Thread.sleep(100);
+                System.out.println("[" + Thread.currentThread().getName() + "] Task DONE!");
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        };
+
+        IntStream.range(0, 24).forEach(i -> threadPoolExecutor.execute(task));
+
+        shutdown(counter, threadPoolExecutor);
+
+        System.out.println("[" + Thread.currentThread().getName() + "] DONE");
+    }
+}
