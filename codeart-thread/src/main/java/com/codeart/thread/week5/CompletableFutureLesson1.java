15fa160c859c78e11eda42841b61dad2406894e9
Adam Wojcicki <adamwojcicki1991@gmail.com>
2020-08-13 13:22:37 +0200

    Asynchronous Completable Future part 1 in Threads - CodeArt module 5.6

---
+++ codeart-thread/src/main/java/com/codeart/thread/week5/CompletableFutureLesson1.java
@@ -0,0 +1,49 @@
+package com.codeart.thread.week5;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+
+public class CompletableFutureLesson1 {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        System.out.println("[" + Thread.currentThread().getName() + "] Starting...");
+
+        CompletableFuture<Long> completableFuture = new CompletableFuture<>();
+        CompletableFuture<String> future = CompletableFuture.completedFuture(" I've already got this one!");
+        CompletableFuture<Long> longCompletableFuture = CompletableFuture.supplyAsync(() -> {
+            try {
+                System.out.println("[" + Thread.currentThread().getName() + "] Doing hard work...");
+                Thread.sleep(500);
+                completableFuture.complete(42L);
+                return 123L;
+            } catch (InterruptedException e) {
+                throw new IllegalStateException(e);
+            }
+        });
+
+        Runnable task = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    System.out.println("[" + Thread.currentThread().getName() + "] Doing hard work...");
+                    Thread.sleep(500);
+                    completableFuture.complete(42L);
+                } catch (InterruptedException e) {
+                    throw new IllegalStateException(e);
+                }
+            }
+        };
+
+
+        Long result = longCompletableFuture.get();
+//        String result = future.get();
+
+//        Executor executor = Executors.newFixedThreadPool(4);
+//        executor.execute(task);
+
+//        Long result = completableFuture.get();
+
+        System.out.println("[" + Thread.currentThread().getName() + "] Result is: " + result);
+
+        System.out.println("[" + Thread.currentThread().getName() + "] DONE");
+    }
+}
